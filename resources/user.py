005160ed681641156c56d29c19128c5b33c79377
ttttttwt <100847404+ttttttwt@users.noreply.github.com>
2023-04-17 14:01:58 +0700

    add send email

--- resources/user.py
+++ resources/user.py
@@ -1,30 +1,51 @@
+import os
+import requests
+
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy import or_
 from flask_jwt_extended import create_access_token, get_jwt, jwt_required, create_refresh_token,get_jwt_identity
 
 from db import db
 from models import UserModel
-from schemas import UserSchema
+from schemas import UserSchema, UserRegisterSchema
 from blocklist import BLOCKLIST
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    api_key = str(os.getenv("MAILGUN_API_KEY"))
+    return requests.post(
+        f"https://api.mailgun.net/v3/{domain}/messages",
+        auth=("api", api_key),
+        data={
+            "from": f"hello101 <mailgun@{domain}>",
+            "to": [to],
+            "subject": subject,
+            "text": body,
+        },
+    )
+
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
         if UserModel.query.filter(UserModel.username == user_data["username"]).first():
             abort(409, message="A user with that username already exists.")
 
         user = UserModel(
             username=user_data["username"],
+            email=user_data["email"],
             password=pbkdf2_sha256.hash(user_data["password"]),
         )
         db.session.add(user)
         db.session.commit()
+        
+        send_simple_message(user_data["email"], "Welcome to the store", "You have successfully registered.")
 
         return {"message": "User created successfully."}, 201
     
